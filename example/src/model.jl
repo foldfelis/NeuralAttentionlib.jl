b2ffbed525d7f7327533e95e2c3446c3cffb04c4
JingYu Ning <foldfelis@gmail.com>
2021-08-19 15:52:38 +0800

    add validate

--- example/src/model.jl
+++ example/src/model.jl
@@ -10,8 +10,8 @@ else
     device = cpu
 end
 
-function load_data()
-    train_x, train_y = CIFAR10.traindata()
+function load_data(mode=:train)
+    train_x, train_y = mode === :train ? CIFAR10.traindata() : CIFAR10.testdata()
 
     return Float32.(train_x) |> device, Flux.onehotbatch(train_y, 0:9) |> device
 end
@@ -50,7 +50,14 @@ end
 m = vgg16()
 loss(𝐱, y) = Flux.logitcrossentropy(m(𝐱), y)
 
-train_loader = Flux.DataLoader(load_data(), batchsize=128, shuffle=true)
-data = [(𝐱, 𝐲) for (𝐱, 𝐲) in train_loader] |> device
+train_loader = Flux.DataLoader(load_data(:train), batchsize=128, shuffle=true)
+test_loader = Flux.DataLoader(load_data(:test), batchsize=128, shuffle=false)
+train_data = [(𝐱, 𝐲) for (𝐱, 𝐲) in train_loader] |> device
 
-Flux.@epochs 500 @time(Flux.train!(loss, params(m), data, Flux.ADAM(3f-4)))
+function validate()
+    validation_losses = [loss(device(𝐱), device(𝐲)) for (𝐱, 𝐲) in test_loader]
+    @info "loss: $(sum(validation_losses)/length(test_loader))"
+end
+
+call_back = Flux.throttle(validate, 60, leading=false, trailing=true)
+Flux.@epochs 500 @time(Flux.train!(loss, params(m), train_data, Flux.ADAM(3f-4), cb=call_back))
